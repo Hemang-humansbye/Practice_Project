36f06685f99a5b3081421311427cc26915592a7d
Hemang-humansbye <124909919+Hemang-humansbye@users.noreply.github.com>
2024-11-05 09:36:00 +0530

    4-11-24

---
+++ server/controllers/doctorsDetailsController.js
@@ -0,0 +1,30 @@
+const mongoose = require("mongoose");
+
+const doctorSchema = new mongoose.Schema({
+    name: {
+        type: String,
+        required: true,
+    },
+    speciality: {
+        type: String,
+        required: true,
+    },
+    phoneNumber: {
+        type: String,
+        required: true,
+    },
+    experience: {
+        type: Number, // years of experience
+        required: true,
+    },
+    address: {
+        type: String,
+        required: true,
+    },
+}, {
+    timestamps: true,
+});
+
+const Doctor = mongoose.model("Doctor", doctorSchema);
+
+module.exports = Doctor;
--- server/controllers/userController.js
+++ server/controllers/userController.js
@@ -26,4 +26,26 @@ const registerUser = asyncHandler(async(req , res)=>{
     });
     res.status(201).json({ message : "user registered succesfully" , user});
 })
-module.exports = { registerUser}
+const loginUser = asyncHandler(async (req, res) => {
+    const { email, password } = req.body;
+    if (!email || !password) {
+        res.status(400);
+        throw new Error("Please provide email and password");
+    }
+    const user = await User.findOne({ email });
+    if (user && (await bcrypt.compare(password, user.password))) {
+        res.status(200).json({
+            message: "User logged in successfully",
+            user: {
+                id: user._id,
+                firstName: user.firstName,
+                lastName: user.lastName,
+                email: user.email,
+            },
+        });
+    } else {
+        res.status(401);
+        throw new Error("Invalid email or password");
+    }
+});
+module.exports = { registerUser, loginUser}
